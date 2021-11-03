c3377c1d5712a17072b89511ffc419f770a9aeb1
Melissa McKay <melissajmckay@gmail.com>
2021-11-03 13:48:57 -0600

    Fix

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,6 @@
 # include <stdio.h>
 
 int main() {
-  printf("\n\n");
-  printf("Oh no!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-  printf("Melissa was here! <insert scary music>\n");
+  printf("Hello World!");
   return 0;
 }
