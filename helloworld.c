99d5162c8db061457049d2b2ab80df067804c5de
Melissa McKay <melissajmckay@gmail.com>
2022-08-17 05:34:05 -0600

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,14 @@
 # include <stdio.h>
 
+#int main() {
+#  printf("\n\n");
+#  printf("Oh no!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+#  printf("Melissa was here! <insert scary music>\n");
+#  return 0;
+#}
+
 int main() {
-  printf("\n\n");
-  printf("Oh no!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-  printf("Melissa was here! <insert scary music>\n");
+  printf("Hello World!");
   return 0;
 }
-
-//int main() {
-//  printf("Hello World!");
-//  return 0;
-//}
 
